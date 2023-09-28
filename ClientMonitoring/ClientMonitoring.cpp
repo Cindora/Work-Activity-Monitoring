6a9735fa84cb4c164a321b0058a59b11e39d0649
Cindora <st111245@student.spbu.ru>
2023-09-28 19:07:24 +0300

    Add Readme, Initiate use of the Winsock, Get the current address

--- ClientMonitoring/ClientMonitoring.cpp
+++ ClientMonitoring/ClientMonitoring.cpp
@@ -1,5 +1,40 @@
-﻿#include <iostream>
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+#include <winSock2.h>
+#include <WS2tcpip.h>
+
+
+using namespace std;
 
 int main()
 {
+	WSADATA wsaData;
+	ADDRINFO hints;
+	ADDRINFO* addrResult = NULL;
+
+	unsigned int result;
+
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (result) {
+		cout << "WSA Startup failed. Result: " << result << endl;
+		return 1;
+	}
+
+	
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	result = getaddrinfo("localhost", "111", &hints, &addrResult);
+
+	if (result) {
+		cout << "Getaddrinfo failed. Result: " << result << endl;
+		WSACleanup();
+		return 1;
+	}
+
 }
---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Client-Server application for monitoring work activity
+
+## Features done:
+
+
+## Features in progress (and TODOs):
+
+- Client preparation
+- Server preparation
+- Client-server messaging
+- Provide work with multiple clients
+- Client: Collect data (domain/machine/ip/user), last activity
+- Client: Send data to server
+- Server: Writing database, saving client data to the database
+- Server: List of all connected clients
+- Server: Screenshot request
+- Client: Screenshot (Receive screenshot request, execute screenshot, send screenshot to server)
+- Server: Screenshot (Receiving and saving a screenshot, entering information about it into the database)
+- Client: Quiet startup at login, running in background mode
